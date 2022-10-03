2c321722c87c9136b3180e25d88b0e2a78fd3299
apelbeku <syauqyalhaq5@gmail.com>
2022-10-03 19:01:44 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#172048',
+        'secondary': '#007299',
+        'gray': '#C4C4C4'
+      },
+    },
+  },
+  plugins: [],
+}
